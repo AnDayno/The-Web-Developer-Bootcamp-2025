7b8086193b6308df3961d3c48ec92318633df7e3
An <162073486+AnDayno@users.noreply.github.com>
2025-07-25 05:05:55 +0800

    Added If Statement

---
+++ Courses/Javascript/2_Conditionals/app.js
@@ -0,0 +1,9 @@
+let random = Math.floor(Math.random() * 2) + 1;
+console.log(random);
+if (random < 2){
+    console.log("YOUR NUMBER IS LESS THAN 1!!");
+};
+
+if (random > 1){
+    console.log("YOUR NUMBER IS GREATER THAN 1")
+}
