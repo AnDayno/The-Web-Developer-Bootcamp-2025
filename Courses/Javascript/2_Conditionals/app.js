80183fc4d70f9978c8f5a053012e792dd73fe48f
An <162073486+AnDayno@users.noreply.github.com>
2025-07-28 09:24:08 +0800

    Added else if

--- Courses/Javascript/2_Conditionals/app.js
+++ Courses/Javascript/2_Conditionals/app.js
@@ -1,9 +1,33 @@
-let random = Math.floor(Math.random() * 2) + 1;
-console.log(random);
-if (random < 2){
-    console.log("YOUR NUMBER IS LESS THAN 1!!");
-};
+// let random = Math.floor(Math.random() * 2) + 1;
+// console.log(random);
+// if (random < 2){
+//     console.log("YOUR NUMBER IS LESS THAN 1!!");
+// };
 
-if (random > 1){
-    console.log("YOUR NUMBER IS GREATER THAN 1")
+// if (random > 1){
+//     console.log("YOUR NUMBER IS GREATER THAN 1");
+// }
+
+// const dayOfWeek = "Friday"
+
+// if (dayOfWeek === "Monday"){
+//     console.log("WHY IS IT MONDAY?");
+// }
+// else if (dayOfWeek === "Saturday"){
+//     console.log("Nice");
+// }
+// else if (dayOfWeek === "Friday"){
+//     console.log("Oke");
+// }
+
+const age = 3;
+
+if (age < 5 ){
+    console.log("You are a baby. You get in for free!")
+}
+else if (age < 10){
+    console.log("You are a child. You pay $10")
+} 
+else if (age < 65){
+    console.log("You are an adult. You pay $20")
 }
