955d8eb3c07542bc7495046b135eb6664e9f5dae
An <162073486+AnDayno@users.noreply.github.com>
2025-08-08 12:47:09 +0800

    Updated Conditionals

--- Courses/Javascript/2_Conditionals/app.js
+++ Courses/Javascript/2_Conditionals/app.js
@@ -26,15 +26,15 @@
 
 // const age = 3;
 
-// if (age < 5 ){
-//     console.log("You are a baby. You get in for free!")
-// }
-// else if (age < 10){
-//     console.log("You are a child. You pay $10")
-// } 
-// else if (age < 65){
-//     console.log("You are an adult. You pay $20")
-// }
-// else{
-//     console.log("You are a senior. You pay $10")
-// }
+if (age < 5 ){
+    console.log("You are a baby. You get in for free!")
+}
+else if (age < 10){
+    console.log("You are a child. You pay $10")
+} 
+else if (age < 65){
+    console.log("You are an adult. You pay $20")
+}
+else{
+    console.log("You are a senior. You pay $10")
+}
