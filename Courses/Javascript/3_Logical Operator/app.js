2d9972d152ba0b5a696dc2eefc1633adf5538664
An <162073486+AnDayno@users.noreply.github.com>
2025-08-09 18:39:36 +0800

    Added Logical Operator

---
+++ Courses/Javascript/3_Logical Operator/app.js
@@ -0,0 +1,9 @@
+const password = prompt("Enter your password");
+
+if (password.length >= 6 && password.indexOf(" ") == -1)
+{
+    console.log("Valid Password");
+}else
+{
+    console.log("Incorrect format for password");
+}
