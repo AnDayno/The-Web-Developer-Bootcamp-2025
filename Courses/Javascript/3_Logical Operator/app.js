af46ddc4fd740d0584a85d398bfeb6ccba0c8eb8
An <162073486+AnDayno@users.noreply.github.com>
2025-08-10 21:45:37 +0800

    Added OR Logical Operator

--- Courses/Javascript/3_Logical Operator/app.js
+++ Courses/Javascript/3_Logical Operator/app.js
@@ -1,9 +1,24 @@
-const password = prompt("Enter your password");
+// ---------- && (AND) LOGICAL OPERATOR ----------
+// const password = prompt("Enter your password");
 
-if (password.length >= 6 && password.indexOf(" ") == -1)
-{
-    console.log("Valid Password");
-}else
-{
-    console.log("Incorrect format for password");
-}
+// if (password.length >= 6 && password.indexOf(" ") == -1)
+// {
+//     console.log("Valid Password");
+// }else
+// {
+//     console.log("Incorrect format for password");
+// }
+
+// ---------- || (OR) LOGICAL OPERATOR ----------
+// const age = 90;
+// if (age >= 0 && age < 5 || age >= 65) {
+// 	console.log("FREE");
+// } else if (age >=5 && age < 10) {
+// 	console.log("$10");
+// } else if (age >= 10 && age < 65) {
+// 	console.log("$20");
+// } else {
+//     console.log("Invalid Age!")
+// }
+
+
