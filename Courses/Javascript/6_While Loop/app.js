2b86498b71dcd4185af1a26b3e2e7e2c91685c05
An <162073486+AnDayno@users.noreply.github.com>
2025-08-14 11:46:53 +0800

    Introduced break;

--- Courses/Javascript/6_While Loop/app.js
+++ Courses/Javascript/6_While Loop/app.js
@@ -4,10 +4,18 @@
 //     console.log(count)
 // }
 
-const secret = "BabyHippo";
+// const secret = "BabyHippo";
 
-let guess = prompt("Enter the secret code...")
-while (guess != secret){
-    guess = prompt("Try Again")
+// let guess = prompt("Enter the secret code...")
+// while (guess != secret){
+//     guess = prompt("Try Again")
+// }
+// console.log("Congrats you got the secret!!")
+
+let input = prompt("Say something")
+
+while(true){
+    input = prompt(input)
+    if (input.toLowerCase() === "stop copying me") break;
 }
-console.log("Congrats you got the secret!!")
+console.log("Okay")
