6968ce29515b6aa3fc076e3f65e810173e43cf83
An <162073486+AnDayno@users.noreply.github.com>
2025-08-18 18:44:33 +0800

    Added for of loops

---
+++ Courses/Javascript/7_For of Loops/app.js
@@ -0,0 +1,17 @@
+// const seatingChart = [
+// 	["Kristen", "Erik", "Namita"],
+// 	["Geoffrey", "Juanita", "Oliver", "Kevin"],
+// 	["Yuma", "Sakura", "Jack", "Erika"],
+// ];
+
+// ---------- Iterating Array ----------
+// for (let row of seatingChart){
+//     for(let student of row){
+//         console.log(student);
+//     } 
+// }
+
+// ---------- Iterating String ----------
+for (let char of 'Hello World'){
+    console.log(char);
+}
