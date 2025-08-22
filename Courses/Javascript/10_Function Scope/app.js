80e6826c6494c3c53c753e0f6d4f8c03260f96b8
An <162073486+AnDayno@users.noreply.github.com>
2025-08-22 15:10:33 +0800

    Added Lexical Scope

--- Courses/Javascript/10_Function Scope/app.js
+++ Courses/Javascript/10_Function Scope/app.js
@@ -19,3 +19,18 @@
 // console.log(radius); //8
 // console.log(PI); //NOT DEFINED
 // console.log(circ); //NOT DEFINED
+
+// ---------- Lexical Scope ----------
+// An inner function nested inside of some parent function has access to the scope
+// or to the variable to find in the scope of that outer function
+
+// function outer() {
+//     let hero = "Black Panther";
+
+//     function inner() {
+//         let cryForHelp = `${hero}, please save me!`;
+//         console.log(cryForHelp);
+//     }
+//     inner();
+// }
+// outer(); // "Black Panther, please save me!"
