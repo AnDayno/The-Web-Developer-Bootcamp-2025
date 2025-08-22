ab96bbd716382a0a6730651ab77b50d78bdd4273
An <162073486+AnDayno@users.noreply.github.com>
2025-08-22 15:02:43 +0800

    Added block scopes

--- Courses/Javascript/10_Function Scope/app.js
+++ Courses/Javascript/10_Function Scope/app.js
@@ -7,4 +7,15 @@
 
 // msg; //NOT DEFINED!
 
+// ---------- Block Scope ----------
+// PI & circ are scoped to the BLOCK
+// let radius = 8
 
+// if (radius > 0){
+//     const PI = 3.14;
+//     let circ = 2 * PI * radius
+// } // Inside the curly braces are called blocks (except on function)
+
+// console.log(radius); //8
+// console.log(PI); //NOT DEFINED
+// console.log(circ); //NOT DEFINED
