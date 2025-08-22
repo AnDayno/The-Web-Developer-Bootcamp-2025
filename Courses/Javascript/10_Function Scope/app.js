d32b5b9da5fafdc4e3c7db8081b17317e657cd9f
An <162073486+AnDayno@users.noreply.github.com>
2025-08-22 15:02:29 +0800

    Added function scopes

---
+++ Courses/Javascript/10_Function Scope/app.js
@@ -0,0 +1,10 @@
+// ---------- Function Scope ----------
+// msg is scoped to the helpMe function
+// function helpMe(){
+//     let msg = 'Im on fire!';
+//     msg; // 'I'm on fire!'
+// }
+
+// msg; //NOT DEFINED!
+
+
