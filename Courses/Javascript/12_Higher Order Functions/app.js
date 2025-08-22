c7cd7cdea3e426dad9f92a174234f4b04733cdda
An <162073486+AnDayno@users.noreply.github.com>
2025-08-22 15:55:51 +0800

    Updated High Order Functions

--- Courses/Javascript/12_Higher Order Functions/app.js
+++ Courses/Javascript/12_Higher Order Functions/app.js
@@ -1,13 +1,27 @@
 // ---------- Accept other functions as arguments ----------
-function callTwice(func){
-    func();
-    func();
+// function callTwice(func){
+//     func();
+//     func();
+// }
+
+// function laugh() {
+//     console.log("HAHAHA");
+// } 
+
+// callTwice(laugh); // pass a function as an arg!
+//"HAHAHA"
+//"HAHAHA"
+
+// ---------- Return a function ----------
+function makeBetweenFunc(min, max){
+    return function(num){
+        return num > min && num < max;
+    }
 }
 
-function laugh() {
-    console.log("HAHAHA");
-} 
-
-callTwice(laugh); // pass a function as an arg!
-//"HAHAHA"
-//"HAHAHA"
+const isChild = makeBetweenFunc(0,18);
+const isAdult = makeBetweenFunc(19,27);
+console.log(isChild(1)); // True
+console.log(isChild(19)); // False
+console.log(isAdult(25)); // True
+console.log(isAdult(30)); // False
