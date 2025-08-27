48b8a555fafaef6ab17d7309002fa9dfff7d4152
An <162073486+AnDayno@users.noreply.github.com>
2025-08-28 01:56:29 +0800

    Arrow Function Implicit return

---
+++ Courses/Javascript/19_Arrow Function Implicit return/app.js
@@ -0,0 +1,10 @@
+// const rollDie = () => { //arrow function
+// 	return Math.floor(Math.random() * 6) + 1;
+// }
+
+// const rollDie = () => ( //Implicit return
+// 	Math.floor(Math.random() * 6) + 1;
+// )
+
+//one-liner implicit return
+const rollDie = () =>  Math.floor(Math.random() * 6) + 1;
