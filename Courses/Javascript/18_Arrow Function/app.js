f47eb7eebf1d3a6c759c5fb769427634d724fc73
An <162073486+AnDayno@users.noreply.github.com>
2025-08-28 01:46:23 +0800

    Added Arrow Function

---
+++ Courses/Javascript/18_Arrow Function/app.js
@@ -0,0 +1,15 @@
+const add = (x,y) => {
+	return x + y;
+}
+
+const subtract = (x,y) => {
+	return x - y;
+}
+
+const square = (x) => {
+	return x * x;
+}
+
+const rollDie = () => {
+	return Math.floor(Math.random() * 6) + 1;
+}
