07471eeeab6fb18e808aa44bdf4bff3b19af392b
An <162073486+AnDayno@users.noreply.github.com>
2025-09-01 21:22:41 +0800

    Added Filter Method

---
+++ Courses/Javascript/21_Filter Method/app.js
@@ -0,0 +1,60 @@
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99,
+        year: 1984
+    },
+    {
+        title: 'Sharknado',
+        score: 35,
+        year: 2013
+    },
+    {
+        title: '13 Going On 30',
+        score: 70,
+        year: 2004
+    },
+    {
+        title: 'Stand By Me',
+        score: 85,
+        year: 1986
+    },
+    {
+        title: 'Waterworld',
+        score: 62,
+        year: 1995
+    },
+    {
+        title: 'Jingle All The Way',
+        score: 71,
+        year: 1996
+    },
+    {
+        title: 'Parasite',
+        score: 95,
+        year: 2019
+    },
+    {
+        title: 'Notting Hill',
+        score: 77,
+        year: 1999
+    },
+    {
+        title: 'Alien',
+        score: 90,
+        year: 1979
+    }
+]
+
+const goodMovies = movies.filter(m => {
+    return m.score > 80
+})
+
+const badMovies = movies.filter(m => m.score < 70)
+console.log(badMovies);
+
+const recentMovies = movies.filter(m => m.year > 2000)
+console.log(recentMovies);
+
+const niceMovies = movies.filter(m => m.score > 70).map(m => m.title);
+console.log(niceMovies);
